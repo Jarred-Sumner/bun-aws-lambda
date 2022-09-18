e08041e49b308fd8d012117ae039afb18ad0ed3f
Jarred Sumner <709451+Jarred-Sumner@users.noreply.github.com>
2022-09-18 05:25:48 -0700

    Update function.js

--- function.js
+++ function.js
@@ -1,5 +1,19 @@
 export default {
-  fetch(req) {
-    return new Response("Hello world!: " + req.url);
+  async fetch(req) {
+    if ((parseInt(req.headers.get("Content-Length") || "0") || 0) > 0) {
+      const body = await req.text();
+      return new Response(body, {
+        headers: {
+          ...Object.fromEntries(req.headers.entries()),
+          "content-type": req.headers.get("Content-Type") || "text/plain",
+        },
+      });
+    }
+
+    return new Response("Hello World", {
+      headers: {
+        "content-type": "text/plain",
+      },
+    });
   },
 };
